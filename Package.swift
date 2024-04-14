87469eb157dc37caab59c729c5dd09f1b2fedd73
ahmediosDeveloper <ahmedaeg1993@hotmail.com>
2024-04-14 16:55:33 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AmazonIVSBroadcast",
+    products: [
+        .library(
+            name: "AmazonIVSBroadcast",
+            targets: ["AmazonIVSBroadcast"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "AmazonIVSBroadcast",
+            url:"https://github.com/ahmediosDeveloper/AmazonIVSBroadcast/releases/tag/1.16.0/AmazonIVSBroadcast.xcframework.zip",
+            checksum: "be6987f5fa941a01b8dac29299c8c145cd103ca261264ec7fae37ad9224aa3c6"
+        )
+    ]
+)
