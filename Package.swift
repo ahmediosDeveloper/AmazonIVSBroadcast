6c40829c23a990f5db9f70ec6a117e4f809d487b
ahmediosDeveloper <ahmedaeg1993@hotmail.com>
2024-04-14 16:59:29 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmazonIVSBroadcast",
-            url:"https://github.com/ahmediosDeveloper/AmazonIVSBroadcast/releases/tag/1.16.0/AmazonIVSBroadcast.xcframework.zip",
+            url:"https://github.com/ahmediosDeveloper/AmazonIVSBroadcast/releases/download/1.16.0/AmazonIVSBroadcast.xcframework.zip",
             checksum: "be6987f5fa941a01b8dac29299c8c145cd103ca261264ec7fae37ad9224aa3c6"
         )
     ]
